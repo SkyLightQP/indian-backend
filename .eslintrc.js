c34f536cccff22d129af7077afb603e4ab82f2c8
DaegyeomHa <combbm@naver.com>
2021-08-30 11:58:05 +0900

    feat: project init

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'airbnb-base', 'eslint-config-prettier'],
+  rules: {
+    'arrow-body-style': 'off',
+    'comma-dangle': 'off',
+    'object-curly-newline': 'off',
+    'linebreak-style': 'off',
+    'no-plusplus': 'off',
+    'no-continue': 'off',
+
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error']
+  }
+};
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import log4js from 'log4js';
+
+export const app = express();
+export const logger = log4js.getLogger();
+
+dotenv.config();
+
+log4js.configure({
+  appenders: {
+    console: {
+      type: 'console'
+    },
+    default: {
+      type: 'file',
+      filename: 'logs/log.log',
+      pattern: '-yyyy-MM-dd',
+      compress: true
+    }
+  },
+  categories: {
+    default: {
+      appenders: ['default', 'console'],
+      level: 'DEBUG'
+    }
+  }
+});
+logger.level = 'ALL';
+
+app.set('trust proxy', true);
+app.use(helmet());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());

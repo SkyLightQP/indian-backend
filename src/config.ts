db2178fa4e6587a0777a574881d3ab4b9b845d29
DaegyeomHa <combbm@naver.com>
2021-08-31 10:48:53 +0900

    feat: add config file

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  DATABASE_HOST: process.env.DATABASE_HOST ?? 'localhost',
+  DATABASE_PORT: Number(process.env.DATABASE_PORT) ?? 3306,
+  DATABASE_USERNAME: process.env.DATABASE_USERNAME ?? 'user',
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD ?? '',
+  DATABASE_DB: process.env.DATABASE_DB ?? 'indian'
+};

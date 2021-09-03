546f07c9c06d57596d33aa50515360caa681dd96
DaegyeomHa <combbm@naver.com>
2021-09-03 09:36:29 +0900

    feat: add USER_ALREADY_EXISTS error code

--- src/common/error/errorCode.ts
+++ src/common/error/errorCode.ts
@@ -11,5 +11,9 @@ export const ErrorCode = createError({
   USER_NOT_FOUND: {
     message: '존재하지 않는 사용자입니다.',
     status: 404
+  },
+  USER_ALREADY_EXISTS: {
+    message: '이미 사용 중인 아이디입니다.',
+    status: 409
   }
 });

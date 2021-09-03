2e683f80312128f3919d1d11be737956eeab1c24
DaegyeomHa <combbm@naver.com>
2021-09-03 09:54:04 +0900

    feat: add env value into config

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,9 @@ export const config = {
   DATABASE_HOST: process.env.DATABASE_HOST ?? 'localhost',
   DATABASE_PORT: Number(process.env.DATABASE_PORT) ?? 3306,
   DATABASE_USERNAME: process.env.DATABASE_USERNAME ?? 'user',
-  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD ?? '',
-  DATABASE_DB: process.env.DATABASE_DB ?? 'indian'
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD ?? 'password',
+  DATABASE_DB: process.env.DATABASE_DB ?? 'indian',
+  CORS: process.env.CORS ?? 'http://localhost:3000',
+  SALT: process.env.SALT ?? 1,
+  SESSION_SECRET: process.env.SESSION_SECRET ?? 'secret'
 };

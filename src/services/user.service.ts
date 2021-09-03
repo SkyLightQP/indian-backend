c23b473621c97b1b8556a492896c01de2ca70518
DaegyeomHa <combbm@naver.com>
2021-09-03 09:36:50 +0900

    feat: implement UserService about create, update and delete

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,6 +5,19 @@ import { ErrorCode } from '../common/error/errorCode';
 
 const userRepository = getRepository(User);
 
+interface CreateUserProps {
+  id: string;
+  password: string;
+  email: string;
+  nickname: string;
+}
+
+interface UpdateUserProps {
+  password: string;
+  email: string;
+  nickname: string;
+}
+
 export const getUsers = async (): Promise<User[]> => {
   return userRepository.find();
 };
@@ -27,4 +40,33 @@ export const getUserById = async (id: string): Promise<User> => {
   });
   if (!user) throw new HttpException(ErrorCode.USER_NOT_FOUND.message, ErrorCode.USER_NOT_FOUND.status);
   return user;
+};
+
+export const createUser = async (props: CreateUserProps): Promise<User> => {
+  const user = await userRepository.findOne({
+    where: {
+      id: props.id
+    }
+  });
+  if (user) throw new HttpException(ErrorCode.USER_ALREADY_EXISTS.message, ErrorCode.USER_ALREADY_EXISTS.status);
+
+  return userRepository.create(props);
+};
+
+export const updateUser = async (uuid: string, data: Partial<UpdateUserProps>): Promise<User> => {
+  const user = await getUserByUuid(uuid);
+  const { raw } = await userRepository.update(
+    {
+      uuid: user.uuid
+    },
+    data
+  );
+  return raw[0];
+};
+
+export const deleteUser = async (uuid: string): Promise<void> => {
+  const user = await getUserByUuid(uuid);
+  await userRepository.softDelete({
+    uuid: user.uuid
+  });
 };

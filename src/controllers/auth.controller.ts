3c51ef5f426dcff00090e210876fcdfb37c8914f
DaegyeomHa <combbm@naver.com>
2021-09-03 11:30:49 +0900

    feat: implement auth controller and apply session

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,92 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import passport from 'passport';
+import { ErrorCode } from '../common/error/errorCode';
+import { logger } from '../index';
 
 export const CONTROLLER_NAME = '/auth';
 const router = express.Router();
+
+router.post('/login', (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate('login', async (error, user, info) => {
+    if (error) {
+      res.status(ErrorCode.SERVER_ERROR.status).json({
+        data: null,
+        error: ErrorCode.SERVER_ERROR.message
+      });
+      return;
+    }
+
+    if (info) {
+      const errorMessage = JSON.parse(info.message);
+      res.status(errorMessage.status).json({
+        data: null,
+        error: errorMessage.message
+      });
+      return;
+    }
+
+    req.login(user, (err) => {
+      if (err) {
+        res.status(ErrorCode.SERVER_ERROR.status).json({
+          data: null,
+          error: ErrorCode.SERVER_ERROR.message
+        });
+        return;
+      }
+
+      logger.info(`${user.uuid} Login`);
+
+      const result = user;
+      result.password = null;
+
+      res.status(200).json({
+        data: result,
+        error: null
+      });
+    });
+  })(req, res, next);
+});
+
+router.post('/register', async (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate('register', async (error, user, info) => {
+    if (error) {
+      res.status(ErrorCode.SERVER_ERROR.status).json({
+        data: null,
+        error: ErrorCode.SERVER_ERROR.message
+      });
+      return;
+    }
+
+    if (info) {
+      const errorMessage = JSON.parse(info.message);
+      res.status(errorMessage.status).json({
+        data: null,
+        error: errorMessage.message
+      });
+      return;
+    }
+
+    res.status(201).json({
+      data: user,
+      error: null
+    });
+
+    logger.info(`${user.uuid} Register`);
+  })(req, res, next);
+});
+
+router.delete('/logout', (req: express.Request, res: express.Response) => {
+  const result = req.user;
+
+  if (!result) return;
+
+  logger.info(`${result.uuid} Logout`);
+  req.logout();
+
+  res.status(200).json({
+    data: result,
+    error: null
+  });
+});
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 import log4js from 'log4js';
+import passport from 'passport';
+import session from 'express-session';
 import { db } from './database';
 import { registerPassport } from './auth';
 import { config } from './config';
@@ -38,6 +40,20 @@ app.use(helmet());
 app.use(cors({ origin: config.CORS, credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(
+  session({
+    secret: config.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: Date.now() + 7 * 86400 * 1000
+    }
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 db().catch((e) => {
   logger.error(`데이터베이스 오류: ${e}`);

92ae7b6672fed8303d72b5ba24c9da4c087a6dd9
DaegyeomHa <combbm@naver.com>
2021-09-10 10:34:04 +0900

    fix: 버그 수정

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -4,9 +4,12 @@ import fs from 'fs';
 const router = express.Router();
 
 fs.readdirSync(__dirname).forEach(async (file) => {
-  if (file === 'index.ts' || file === 'index.js') return;
-  const i = await import(`./${file}`);
-  router.use(i.CONTROLLER_NAME, i.default);
+  if (file.includes('.ts') || file.includes('.js')) {
+    if (file !== 'index.ts' && file !== 'index.js') {
+      const i = await import(`./${file}`);
+      router.use(i.CONTROLLER_NAME, i.default);
+    }
+  }
 });
 
 export default router;

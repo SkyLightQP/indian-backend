982d84f99bbd872163b45fe7cef5623cd95868f4
DaegyeomHa <combbm@naver.com>
2021-09-18 22:46:38 +0900

    feat: add error codes

--- src/common/error/errorCode.ts
+++ src/common/error/errorCode.ts
@@ -23,5 +23,29 @@ export const ErrorCode = createError({
   USER_ALREADY_EXISTS: {
     message: '이미 사용 중인 아이디입니다.',
     status: 409
+  },
+  COMPANY_NOT_FOUND: {
+    message: '존재하지 않는 제작사입니다.',
+    status: 404
+  },
+  COMPANY_ALREADY_EXISTS: {
+    message: '이미 추가 된 제작사입니다.',
+    status: 409
+  },
+  COMPANY_BOARD_NOT_FOUND: {
+    message: '존재하지 않는 제작사 정보입니다.',
+    status: 404
+  },
+  COMPANY_BOARD_ALREADY_EXISTS: {
+    message: '이미 추가 된 제작사 정보입니다.',
+    status: 409
+  },
+  GAME_BOARD_NOT_FOUND: {
+    message: '존재하지 않는 게임 정보입니다.',
+    status: 404
+  },
+  GAME_BOARD_ALREADY_EXISTS: {
+    message: '이미 추가 된 게임 정보입니다.',
+    status: 409
   }
 });

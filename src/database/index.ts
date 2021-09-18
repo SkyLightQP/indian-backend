abfde7c049377637dde34b07528fcf07ad74e00b
DaegyeomHa <combbm@naver.com>
2021-09-18 23:20:28 +0900

    fix: 모델 구조 반영

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,6 @@
 import { createConnection, getConnection, Repository } from 'typeorm';
 import { config } from '../config';
 import { User } from '../models/user.model';
-import { Company } from '../models/company.model';
 import { CompanyBoard } from '../models/companyBoard.model';
 import { GameBoard } from '../models/gameBoard.model';
 
@@ -13,11 +12,10 @@ export const db = async (): ReturnType<typeof createConnection> =>
     username: config.DATABASE_USERNAME,
     password: config.DATABASE_PASSWORD,
     database: config.DATABASE_DB,
-    entities: [User, Company, CompanyBoard, GameBoard],
+    entities: [User, CompanyBoard, GameBoard],
     synchronize: process.env.NODE_ENV !== 'production'
   });
 
 export const userRepository = (): Repository<User> => getConnection().getRepository(User);
-export const companyRepository = (): Repository<Company> => getConnection().getRepository(Company);
 export const companyBoardRepository = (): Repository<CompanyBoard> => getConnection().getRepository(CompanyBoard);
 export const gameBoardRepository = (): Repository<GameBoard> => getConnection().getRepository(GameBoard);
